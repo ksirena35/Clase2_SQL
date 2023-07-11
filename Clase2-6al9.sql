f458a28a75922236d2c919432f366de60c0cad65
katherinet <131787792+ksirena35@users.noreply.github.com>
2023-07-11 16:15:46 -0300

    Actualizar Clase2-6al9.sql

--- Clase2-6al9.sql
+++ Clase2-6al9.sql
@@ -1,15 +1,19 @@
+--6.
 insert into  
 	customers (customer_id, company_name, contact_name, contact_title, address, city, region, postal_code, country, phone, fax)
 values ('TANAN', 'katherinet', 'katherine tananta', 'jefa', 'cordoba_3344', 'buenos_aires', 'south', 1133, 'argentina', 1134567890, null);
 
+--7.
 insert into 
 	region (region_id, region_description)
 values (5, 'center');
 		
+--8.
 select *
 from customers c 
 where region is null;
 
+--9.
 select
 product_name,
 coalesce (unit_price, 10) as price
